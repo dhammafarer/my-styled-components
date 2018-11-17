87e3b89c9b6db026ecde31238013dcb0231dd941
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-11-17 21:11:57 +0800

    improve getWithDirection with isEmpty

--- src/theme/primitives/getters.test.ts
+++ src/theme/primitives/getters.test.ts
@@ -1,4 +1,4 @@
-import {identity, map, compose, concat, prop, reduce, either, isNil } from "ramda";
+import { isEmpty, ifElse, always, complement, identity, map, compose, concat, prop, reduce, either, isNil } from "ramda";
 
 const propPrefix = (p:string) => compose(prop, concat(p));
 
@@ -12,16 +12,19 @@ const getDir = (l: string[]) => (p: string) => getEither(makeList(l)(getCode(p))
 
 const template = (key:string, val: string) => `${key}: ${val};`;
 
-const getProperty = (fn: any) => (getter: any) => (property: string) => (props: any) =>  {
-  const val = getter(props);
-  return (val ? template(property, (val)) : "");
-}
+const getProperty = (fn: any) => (getter: any) => (property: string) => (props: any) => ifElse(
+  isNil,
+  always(""),
+  x => template(property, fn(x)))
+(getter(props)
+);
 
 const getDirectionalProperty = (fn: any) => (dp: {dir: string, l: string[]}) => (property: string) =>
   getProperty(fn)(getDir(dp.l)(property))(`${property}-${dp.dir}`);
 
 const getWithDirections = (fn: any) => (dps: any[]) => (property: string) => (props: any) => dps
   .map(d => getDirectionalProperty(fn)(d)(property)(props))
+  .filter(complement(isEmpty))
   .join("\n");
 ;
 
@@ -34,5 +37,10 @@ test("getProperty works for directional properties", () => {
 });
 
 test("getWithDirections", () => {
-  expect(getWithDirections(identity)([{dir: "left", l: ["l","x",""]}])("padding")({pl: 1, px: 2, p: 3})).toBe("padding-left: 1;");
+  expect(getWithDirections(identity)([
+    {dir: "left", l: ["l","x",""]},
+    {dir: "right", l: ["r","x",""]},
+    {dir: "top", l: ["t","y",""]},
+    {dir: "bottom", l: ["b","y",""]},
+  ])("padding")({pl: 1, px: 2})).toBe("padding-left: 1;\npadding-right: 2;");
 });
