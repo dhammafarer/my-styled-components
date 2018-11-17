514c4978942b20be3f8bbbae130b1645a37114ed
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-11-17 20:59:34 +0800

    write getDirectionalProperty

--- src/theme/primitives/getters.test.ts
+++ src/theme/primitives/getters.test.ts
@@ -1,31 +1,30 @@
 import {identity, map, compose, concat, prop, reduce, either, isNil } from "ramda";
 
 const propPrefix = (p:string) => compose(prop, concat(p));
+
 const makeList = (list: string[]) => (p: string) => map(propPrefix(p), list);
 
 const getEither = reduce(either, isNil);
 
-const getDir = (l: string[]) => (p: string) => getEither(makeList(l)(p));
-
 const getCode = (p: string) => p.slice(0,1);
 
-const makeKey = (...ss: string[]) => ss.join("-");
+const getDir = (l: string[]) => (p: string) => getEither(makeList(l)(getCode(p)));
 
-const left = getDir(["l", "x", ""]);
-const right = getDir(["r", "x", ""]);
+const template = (key:string, val: string) => `${key}: ${val};`;
 
-const getProperty = (fn: any) => (n: string) => (getter: any) => (p: string) => (props: any) =>  {
-  const key = makeKey(p,n);
-  const val = fn(getter(getCode(p))(props));
-  return `${key}: ${val};`
+const getProperty = (fn: any) => (getter: any) => (property: string) => (props: any) =>  {
+  const val = getter(props);
+  return (val ? template(property, (val)) : "");
 }
 
-const getLeft = getProperty(identity)("left")(left);
-const getRight = getProperty(identity)("right")(right);
-
-const build = (fns: any) => (prop: string) => (props: any) => fns.map((fn:any) => fn(prop)(props)).join("\n");
+const getDirectionalProperty = (dp: {dir: string, l: string[]}) => (property: string) =>
+  getProperty(identity)(getDir(dp.l)(property))(`${property}-${dp.dir}`);
 
 
-test("getProperty", () => {
-  expect(build([getLeft, getRight])("padding")({pl: 1, px: 2, p: 3})).toBe("padding-left: 1;\npadding-right: 2;");
+test("getProperty works for simple properties", () => {
+  expect(getProperty(identity)(prop("color"))("color")({color: "red"})).toBe("color: red;");
+});
+
+test("getProperty works for directional properties", () => {
+  expect(getDirectionalProperty({l:["l","x",""], dir: "left"})("padding")({pl: 1, px: 2, p: 3})).toBe("padding-left: 1;");
 });
